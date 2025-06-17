11389dc532a3de14817edf1dee0ba99b856550e6
SainathMaddela2409 <23b1201@iitb.ac.in>
2025-06-17 11:04:15 +0530

    Update doc_scanner.py

--- doc_scanner.py
+++ doc_scanner.py
@@ -1,26 +1,42 @@
 import cv2 as cv
 import numpy as np
+import os
 
 from utility import getcontours,reorder,warp_doc
 
-IMAGE_PATH = 'samples/sample1.jpg'
-OUTPUT_PATH = 'output/scanned_sample1.jpg'
+input_folder = 'samples'
+output_folder = 'output'
+
+# Create output folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
 
 kernel = np.ones((5,5))
-img = cv.imread(IMAGE_PATH)
-
-imgc=img.copy()
-gframe = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-gframe = cv.GaussianBlur(gframe,(5,5),1)
-canny = cv.Canny(gframe,10,200)
-dila = cv.dilate(canny,kernel,iterations=2)
-erode = cv.erode( dila,kernel,iterations=1)
-biggest = getcontours(erode)
-if biggest is not None :
-    Doc = warp_doc(img,biggest)
-    cv.imwrite(OUTPUT_PATH, Doc)
-    print('scanned document saved at{OUTPUT_PATH}')
-else :
-    print('could not find contour')
 
 
+# Supported image file extensions
+image_extensions = ('.png', '.jpg', '.jpeg', '.bmp', '.tiff', '.webp')
+
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith(image_extensions):
+        IMAGE_PATH = os.path.join(input_folder, filename)
+        OUTPUT_PATH = os.path.join(output_folder, filename)
+
+        img = cv.imread(IMAGE_PATH)
+        if img is not None:
+            imgc=img.copy()
+            #preprocessing 
+            gframe = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+            gframe = cv.GaussianBlur(gframe,(5,5),1)
+            canny = cv.Canny(gframe,10,200)
+            dila = cv.dilate(canny,kernel,iterations=2)
+            erode = cv.erode( dila,kernel,iterations=1)
+            biggest = getcontours(erode)
+            if biggest is not None :
+                #Warping Doc 
+                Doc = warp_doc(img,biggest)
+                cv.imwrite(OUTPUT_PATH, Doc)
+                print('scanned document saved at output folder')
+            else :
+                print('could not find Document')
+        else:
+            print(f"Failed to read: {IMAGE_PATH}")
